9c1bc6c94cb194768121d2bfdb93a49fe663e597
MiracleChiji <mchiji905@gmail.com>
2025-10-14 22:15:20 +0100

    First commit

---
+++ Arith.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"What would like to convert celsius or fahrenheit?: ";
+    if (fahrenheit){
+
+int Fahrenheit;
+cout<< "Enter value of temperature in Fahrenheit :"<< endl;
+cin>>Fahrenheit;
+float new_value_in_celsius= (5/9)*(Fahrenheit - 32);
+cout <<"The temperature in celsius: " << new_value_in_celsius;}
+
+
+
+
+}
---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int i = 1;
+int j = ++i;
+ i = ++j;
+ cout<<"The value of j is : "<< j <<endl;
+cout<<"The value of i is : "<< i <<endl;
+    
+}
